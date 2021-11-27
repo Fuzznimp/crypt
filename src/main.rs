22d4c66f608f07b62712ed4a2e2742fd829e057a
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-11-27 17:46:27 +0100

    Basic setup for clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::{App, Arg};
+
 fn main() {
-  println!("Hello, world!");
+    let matches = App::new("crypt")
+        .version("0.1.0")
+        .author("Anthony Montaigne. <anthony@montaigne.dev>")
+        .about("Simple CLI to create JWS, JWE and JWKS.")
+        .arg(
+            Arg::new("jws")
+                .short('s')
+                .long("jws")
+                .value_name("SECRET")
+                .about("Generate a JWS using a given secret")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    if let Some(i) = matches.value_of("jws") {
+        println!("Value for input: {}", i);
+    }
 }
